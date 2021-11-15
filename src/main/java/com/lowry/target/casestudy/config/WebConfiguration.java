85c0d507cc36ba5b69242474992a067aa497b62b
dlowry20 <EngGrad2017>
2021-11-14 19:48:47 -0600

    Dave Lowry - enable swagger ui

--- src/main/java/com/lowry/target/casestudy/config/WebConfiguration.java
+++ src/main/java/com/lowry/target/casestudy/config/WebConfiguration.java
@@ -3,15 +3,29 @@ package com.lowry.target.casestudy.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class WebConfiguration {
 
-
     @Bean
-    WebClient webClient() {
+    public WebClient webClient() {
         return WebClient.builder()
                 .baseUrl("https://redsky-uat.perf.target.com/redsky_aggregations/v1/redsky/case_study_v1?key=3yUxt7WltYG7MFKPp7uyELi1K40ad2ys")
+                .build();
+    }
+
+    @Bean
+    public Docket documentation() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
                 .build();
     }
 }
